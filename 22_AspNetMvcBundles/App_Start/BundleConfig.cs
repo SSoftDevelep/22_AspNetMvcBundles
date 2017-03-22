d04357bc407cf8a9fc8c69ba82ce0866eac023a8
SsoftDevelep <saitemresolmaz@gmail.com>
2017-03-23 01:38:50 +0300

    AspNet Mvc-Bundles Kullanimi

---
+++ 22_AspNetMvcBundles/App_Start/BundleConfig.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace _22_AspNetMvcBundles.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            //CSS-StyleBundle
+            bundles.Add(new StyleBundle("~/css/all").Include(
+                "~/Content/bootstrap.css",
+                "~/Content/bootstrap-theme.css",
+                "~/Content/Site.css"));
+
+
+            bundles.Add(new StyleBundle("~/css/basic").Include(
+              "~/Content/bootstrap.css",
+              "~/Content/Site.css"));
+            
+
+            ////JS - ScriptBundle
+            //bundles.Add(new ScriptBundle("~/js/all").IncludeDirectory(
+            //    "~/Scripts", "*.js", true));   //uzantisi .js olan tüm scriptleri ekler. Include Directory ile sub klasorlerdekini de buldu.
+
+
+            //JS-ScriptBundle-CDN kullanimi
+            bundles.UseCdn = true;
+            string jQcdn = "https://code.jquery.com/jquery-2.2.44.js";
+
+            bundles.Add(new ScriptBundle("~/js/all",jQcdn).Include(
+                "~/Scripts/jquery-{version}.js"));
+
+
+            ////JS-ScriptBundle
+            //bundles.Add(new ScriptBundle("~/js/all").Include(
+            //    "~/Scripts/jquery-{version}.js",
+            //    "~/Scripts/bootstrap.js",
+            //    "~/Scripts/modernizr-{version}.js"));
+
+            /*BundleTable.EnableOptimizations = false; *///olusturulan paketlerin minimize edilerek tek dosya olusturularak sayfaya eklenmesini saglar.
+        }
+    }
+}
--- 22_AspNetMvcBundles/Global.asax.cs
+++ 22_AspNetMvcBundles/Global.asax.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using _22_AspNetMvcBundles.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace _22_AspNetMvcBundles
@@ -13,6 +15,7 @@ namespace _22_AspNetMvcBundles
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);  //Bundlelarimizi ekledik.
         }
     }
 }
